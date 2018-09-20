09d9c0bf6710e02fb7d3b0dde0984fe18154f6be
DT <1185192325@qq.com>
2018-09-20 17:27:23 +0800

    Update tbprice.py

--- tbprice.py
+++ tbprice.py
@@ -4,8 +4,8 @@
 @Author:      datadt
 @DateTime:    2018-05-30 15:26:47
 '''
+#tip:有时候采集不到，需要切换到控制模式下查看执行情况，偶尔登录账号试试
 #获取销售价格
-
 from selenium import webdriver
 import requests
 
@@ -17,15 +17,11 @@ def getprice(id):
 	prefs = {"profile.managed_default_content_settings.images": 2}#禁止加载图片，提升效率
 	co.add_experimental_option("prefs", prefs)
 	browser = webdriver.Chrome(chrome_options=co)#,desired_capabilities=desired_capabilities
+	browser.get('https://detail.tmall.hk/hk/item.htm?id='+str(id))
 	try:
-		browser.get('https://detail.tmall.hk/hk/item.htm?id='+str(id))
 		PromoPrice = browser.find_element_by_css_selector('#J_PromoPrice > dd > div > span').text#正常促销价
-		return PromoPrice
+	except:
+		PromoPrice = browser.find_element_by_css_selector('#J_StrPriceModBox > dd > span').text#正常售价
 	finally:
+		return PromoPrice
 		browser.quit()
-
-
-
-
-
-#有时候采集不到，需要切换到控制模式下查看执行情况，偶尔登录账号试试

6bc2b268abfb101363e92187f579ca75f03b3336
DT <1185192325@qq.com>
2018-10-25 19:48:33 +0800

    Update tbpic.py

--- tbpic.py
+++ tbpic.py
@@ -4,8 +4,7 @@
 @Author:      datadt
 @DateTime:    2018-08-08 17:09:03
 '''
-#subject:获取主图way2
-
+#subject:获取主图way3
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,15 +15,22 @@ def tbpicdl(id):
 	url='https://item.taobao.com/item.htm?id='+str(id)
 	soup=BeautifulSoup(requests.get(url).text,'lxml')
 	try:
-		s=soup.find_all('dl',attrs={'class':'tb-prop tm-sale-prop tm-clear tm-img-prop '})[0]#列表内元素
-		pattern=re.compile('//(.*?)_40x40q90.jpg')
-		result=pattern.findall(str(s))[0]#链接多商品的情况下取第一个商品图片链接
-		picurl='https://'+result+'_800x800q90.jpg'
+		try:
+			s=soup.find_all('dl',attrs={'class':'tb-prop tm-sale-prop tm-clear tm-img-prop '})[0]#列表内元素，链接多商品的情况下取第一个商品图片链接
+		except:
+			s=soup.find_all('img',attrs={'id':'J_ImgBooth'})[0]#解决颜色等分类描述没有图片的情况，按照第一张预览图取主图
+		finally:
+			pattern=re.compile('//(.*?)_\d+x\d+q90.jpg')
+			result=pattern.findall(str(s))[0]
+			picurl='https://'+result+'_800x800q90.jpg'
+			urllib.request.urlretrieve(picurl, "%s.jpg" % str(id))
 	except:
-		s=soup.find_all('img',attrs={'id':'J_ImgBooth'})[0]#解决颜色等分类描述没有图片的情况，按照第一张预览图取主图#575745472218
-		pattern=re.compile('//(.*?)_430x430q90.jpg')
-		result=pattern.findall(str(s))[0]
-		picurl='https://'+result+'_800x800q90.jpg'
-	finally:
-		urllib.request.urlretrieve(picurl, "%s.jpg" % str(id))
+		try:
+			pattern=re.compile('//(.*?)_\d+x\d+q90.jpg')
+			result=pattern.findall(str(soup))[0]#直接从网页源获取
+			picurl='https://'+result+'_800x800q90.jpg'
+			urllib.request.urlretrieve(picurl, "%s.jpg" % str(id))
+		except:
+			print('Download failed,Try again!',str(id))#采集过快速、过频繁会出错，打印错误ID，可重来
+
 

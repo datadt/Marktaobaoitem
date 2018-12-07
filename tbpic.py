1974da335bcec20076f76e52f524e0b495830123
DT <1185192325@qq.com>
2018-12-07 14:30:49 +0800

    Update tbpic.py

--- tbpic.py
+++ tbpic.py
@@ -4,33 +4,31 @@
 @Author:      datadt
 @DateTime:    2018-08-08 17:09:03
 '''
-#subject:获取主图way3
+#subject:获取主图way4
 
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-def tbpicdl(id):
+def tbpicdl(id,size=800):
 	url='https://item.taobao.com/item.htm?id='+str(id)
 	soup=BeautifulSoup(requests.get(url).text,'lxml')
 	try:
 		try:
-			s=soup.find_all('dl',attrs={'class':'tb-prop tm-sale-prop tm-clear tm-img-prop '})[0]#列表内元素，链接多商品的情况下取第一个商品图片链接
+			try:
+				s=soup.find_all('ul',attrs={'id':'J_UlThumb'})[0]#解决颜色等分类描述没有图片的情况，按照第一张预览图取主图 way1
+			except:
+				s=soup.find_all('img',attrs={'id':'J_ImgBooth'})[0]#取第一张图 way2
 		except:
-			s=soup.find_all('img',attrs={'id':'J_ImgBooth'})[0]#解决颜色等分类描述没有图片的情况，按照第一张预览图取主图
+			try:
+				s=soup.find_all('dl',attrs={'class':'tb-prop tm-sale-prop tm-clear tm-img-prop '})[0]#列表内元素，链接多商品的情况下取第一个商品图片链接 way3
+			except:
+				s=soup##直接从网页源获取 way4
 		finally:
 			pattern=re.compile('//(.*?)_\d+x\d+q90.jpg')
 			result=pattern.findall(str(s))[0]
-			picurl='https://'+result+'_800x800q90.jpg'
+			picurl='https://'+result+'_'+str(size)+'x'+str(size)+'q90.jpg'
 			urllib.request.urlretrieve(picurl, "%s.jpg" % str(id))
 	except:
-		try:
-			pattern=re.compile('//(.*?)_\d+x\d+q90.jpg')
-			result=pattern.findall(str(soup))[0]#直接从网页源获取
-			picurl='https://'+result+'_800x800q90.jpg'
-			urllib.request.urlretrieve(picurl, "%s.jpg" % str(id))
-		except:
-			print('Download failed,Try again!',str(id))#采集过快速、过频繁会出错，打印错误ID，可重来
-
-
+		print('Download failed,Try again!',str(id))#采集过快速、过频繁会出错，打印错误ID，可重来
